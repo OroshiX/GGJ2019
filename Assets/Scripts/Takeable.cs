21fbfa0fb14830a7fc239f4d902f8fe06608f676
OroshiX <oroshix@users.noreply.github.com>
2019-01-27 04:29:28 +0100

    Mannequin et takable

--- Assets/Scripts/Takeable.cs
+++ Assets/Scripts/Takeable.cs
@@ -4,6 +4,9 @@ public class Takeable : InteractableObject {
     [SerializeField]
     private TypeObject type;
 
+    [SerializeField]
+    private Sprite carriedDark, carriedLight;
+
     public TypeObject getTheType() {
         return type;
     }
